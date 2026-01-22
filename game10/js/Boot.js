df2a66911c1217c818d6b93f1c7bd7b7fe89c3c9
1617play <1617play@gmail.com>
2026-01-22 14:56:51 +0700

    up game10

---
+++ game10/js/Boot.js
@@ -0,0 +1,18 @@
+var MyGame = {};
+MyGame.Boot = function(game) {
+};
+MyGame.Boot.prototype = {
+    preload: function() {
+    },
+    create: function() {
+        this.stage.backgroundColor = '#2d2d2d';
+        this.input.maxPointers = 1;
+
+        this.game.physics.startSystem(Phaser.Physics.ARCADE);
+
+        this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+        this.scale.pageAlignHorizontally = true;
+        this.scale.pageAlignVertically = true;
+        this.state.start('Preloader');
+    }
+};
